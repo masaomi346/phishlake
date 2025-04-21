18e3048363c8981d7383ab34d506f97e3eb89fbb
masaomi346 <36557605+masaomi346@users.noreply.github.com>
2025-04-21 10:38:16 +0900

    Update block-telegram.py

--- docker/mitmproxy/addons/block-telegram.py
+++ docker/mitmproxy/addons/block-telegram.py
@@ -10,6 +10,7 @@ Usage:
 from mitmproxy import ctx, http, log
 from mitmproxy.addons import termlog
 import json
+import datetime
 
 class BlockTelegram:
     def __init__(self):
@@ -26,11 +27,13 @@ class BlockTelegram:
     def response(self, flow):
         telegram_api_domain = 'api.telegram.org'
         if flow.request.pretty_host.endswith(telegram_api_domain):
+            time = datetime.datetime.now()
             data = {}
+            data['time'] = time.strftime("%Y-%m-%d %H:%M:%S")
             data['payload'] = flow.request.get_text()
             data['headers'] = {key: value for (key, value) in flow.request.headers.items()}
             data['url'] = flow.request.pretty_url
-            with open(self.outfile, 'a') as fd:
+            with open(self.outfile, 'a', encoding='utf-8') as fd:
                 json.dump(data, fd)
                 fd.write('\n')
 
